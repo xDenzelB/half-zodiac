57e653de02395bd83089ca4ebc3ffefab3af5403
Denzel <dbartolaba123@gmail.com>
2022-01-25 14:17:47 -0800

    Fixed the header/footer

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import './App.css'
 import Main from './Main/Main'
 import Footer from './Footer/Footer'
 import Header from './Header/Header'
+
 {/* import the zodiac array */}
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Header name='Denzel' />
       <Main />
-      <Header />
-      <Footer />
+      <Footer year='2022' />
       {/* import and use the Header. Main, and Footer components here */}
       {/* the Header component needs a string passed in as the `name` prop */}
       {/* the Footer component needs a year passed in as the `year` prop */}
