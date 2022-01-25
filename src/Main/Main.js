e14cf2c3aef84d0e0bd31663e2a1b52cef9312dc
Denzel <dbartolaba123@gmail.com>
2022-01-24 16:12:53 -0800

    First React

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import backgroundImg from '../background.png'
 import './Main.css'
+import ZodiacCard from '../ZodiacCard/ZodiacCard'
+import { zodiac } from '../data'
 
 export default function Main() {
   return (
     <main style={{ backgroundImage: `url(${backgroundImg})` }}>
+      {zodiac.map((item) => (
+        <ZodiacCard key={item.name} {...item} />
+      )
+      )}
       {/* import the ZodiacCard component */}
       {/* get the zodiac array from props */}
       {/* for each item in the `zodiacs` array props, render a ZodiacCard component, passing in the name and the dates as props to the ZodiacCard component */}
--- src/ZodiacCard/ZodiacCard.js
+++ src/ZodiacCard/ZodiacCard.js
@@ -1,5 +1,13 @@
 import './ZodiacCard.css'
 
-export default function ZodiacCard() {
+export default function ZodiacCard(props) {
   {/* in this component,  use the `name` and `dates` props to render the name and dates of the sign, as well as load the correct image  from the public directory */}
+
+  return (
+    <div className='zodiac-card'>
+      <img src={`/images/${props.name}.png`} />
+      <p className='name'>{props.name}</p>
+      <p>{props.dates}</p>
+    </div>
+  )
 }
